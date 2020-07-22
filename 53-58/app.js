74f9983aed04dd6b75852c4983c92d719e67c8ab
aliabbas <57368553+aliabbasTECH@users.noreply.github.com>
2020-07-22 23:48:13 +0500

    38-67 assignment are complete

---
+++ 53-58/app.js
@@ -0,0 +1,25 @@
+// var modalEle = document.querySelector(".modal");
+// var modalImage = document.querySelector(".modalImage");
+// Array.from(document.querySelectorAll(".ImgThumbnail")).forEach(item => {
+//    item.addEventListener("click", event => {
+//       modalEle.style.display = "block";
+//       modalImage.src = event.target.src;
+//    });
+// });
+// document.querySelector(".close").addEventListener("click", () => {
+//    modalEle.style.display = "none";
+// });
+
+//      <<<<<<<<<<<<<<<<<<<<<<< task2 >>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// var  count = 10;
+// function zoom(){
+//     count = count + 10;
+//     a=count+"px"
+//     document.getElementById("pera").style.fontSize =a
+// }
+// function zoomout(){
+//   count = count - 10;
+//     a=count+"px"
+//     document.getElementById("pera").style.fontSize =a
+//   }
---
+++ 58-67/app.js
@@ -0,0 +1,55 @@
+// // <<<<<<<<<<<<<<<<<<<<<< 1 >>>>>>>>>>>
+// var a=document.getElementById("main-content")
+// console.log(a)
+// //  <<<<<<<<<<<<<< 2 >>>>>>>>>>>>>>>>>>> 
+// var b=document.getElementById("main-content").childNodes
+// console.log(b)
+
+// // <<<<<<<<<<<<<<< 3 >>>>>>>>>>>>>>
+
+// var x = document.getElementsByClassName("render");
+// document.getElementById("ans1").innerHTML = x[0].innerHTML+"<br>"
+// +x[1].innerHTML+"<br>"
+// +x[2].innerHTML+"<br>"
+// +x[3].innerHTML+"<br>"
+// +x[4].innerHTML+"<br>"
+
+// // <<<<<<<<<<<<<<<< 4 >>>>>>>>>>>>>>
+
+// document.getElementById("first-name").value="aliabbas"
+// document.getElementById("last-name").value="aliabbas"
+
+
+
+
+
+//  <<<<<<<<<<<<<<<<<<<<<, task 2  >>>>>>>>>>>>>>>>>>>>>>>
+
+// var c = document.getElementById("form-content").nodeType
+//   document.getElementById("ans2").innerHTML = c
+ 
+
+//   var d = document.getElementById("last-name").nodeType
+//   document.getElementById("ans3").innerHTML = d
+  
+
+//   var d = document.getElementById("last-name").ChildNodes
+//   document.getElementById("ans4").innerHTML = d
+
+//   var d = document.getElementById("lastName").ChildNodes="aliabbas"
+//   document.getElementById("ans5").innerHTML = d
+
+//   var e = document.getElementById("main-content")
+//    document.getElementById("ans6").innerHTML=e.firstChild;
+//    var e = document.getElementById("main-content")
+//    document.getElementById("ans7").innerHTML=e.lastChild;
+
+//    var f = document.getElementById("lastName").previousSibling.innerHTML; 
+//   document.getElementById("ans8").innerHTML = f;
+//    var g = document.getElementById("lastName").nextSibling.innerHTML; 
+//   document.getElementById("ans9").innerHTML = g;
+
+//   var h = document.getElementById("email").parentNode.nodeName;
+  
+//   var i = document.getElementById("email").parentNode.nodeType;
+//   document.getElementById("ans10").innerHTML ="nodename = "+ h +"<br>type = "+ i;
